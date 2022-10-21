94234ee334d54a40a433ef3a797e27ec9faa859e
albertso26 <80046134+albertso26@users.noreply.github.com>
2022-10-21 08:12:23 +0800

    yield to test

---
+++ yieldTest.py
@@ -0,0 +1,17 @@
+def makex(x):
+    while x<6:
+        yield x
+        x  += 1
+        # print (next(n))
+    
+n= makex(0)
+
+
+next (n)
+
+next (n)
+print (next (n))
+next (n)
+print (next (n))
+print (next (n))
+''''''
